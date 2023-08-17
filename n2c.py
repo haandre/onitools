8601ce6e7ac109c4d2a634123d390f74ffcc7499
Andre Haehnel <a.ahehnel@telekom.de>
2023-08-17 08:51:11 +0200

    gui

--- n2c.py
+++ n2c.py
@@ -1,6 +1,10 @@
-from argparse import ArgumentParser
 from names import get_full_name
 from subprocess import Popen, PIPE
+from tkinter import Tk
+from tkinter import ttk
+
+
+labels = {}
 
 
 def generate_name(gender: str = "female") -> str:
@@ -11,28 +15,34 @@ def copy_string_to_clipboard(data: str):
     Popen(('clip'), stdin=PIPE).communicate(data.encode())
 
 
+def get_name(gender: str="female"):
+    name = generate_name(gender=gender)
+    copy_string_to_clipboard(name)
+    labels[gender].configure(text=name)
+
+
+def male():
+    get_name("male")
+
+def female():
+    get_name()
+
+
 def main():
-    parser = ArgumentParser(
-        description='use names module to generate names and push them to clipboard')
-    parser.add_argument('-m', '--male',
-                        help="male frist name, if not set it will be female",
-                        action='store_true',
-                        default=False)
-    parser.add_argument('-s', '--show',
-                        help="print name to console",
-                        action='store_true',
-                        default=False)
-    args = parser.parse_args()
-    if args.male is not None:
-        if args.male:
-            name = generate_name(gender="male")
-        else:
-            name = generate_name(gender="female")
-        copy_string_to_clipboard(name)
-        if args.show is True:
-            print(name)
-    else:
-        print(args)
+    root = Tk()
+    # root.geometry("200x100")
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="Names 2 Cilpboard!").grid(column=0, row=0, columnspan=2)
+    labels["female"] = ttk.Label(frm, text=" ")
+    labels["female"].grid(column=1, row=1)
+    labels["male"] = ttk.Label(frm, text=" ")
+    labels["male"].grid(column=1, row=2)
+    ttk.Button(frm, text="Female", command=female).grid(column=0, row=1)
+    ttk.Button(frm, text="Male", command=male).grid(column=0, row=2)
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=3)
+    ttk.Label(frm, text=" " * 40).grid(column=1, row=3)
+    root.mainloop()
 
 
 if __name__ == "__main__":
